5fa9ffa3541f147732b2f06a51f75225c99cc1c1
ikeroficcial <iker.calvo.gomez@udc.es>
2022-09-20 11:25:28 +0200

    Start e2

---
+++ DS-43-iker.calvo.gomez_ivan.martinezes/src/e2/SocialDistance.java
@@ -0,0 +1,44 @@
+package e2;
+
+public class SocialDistance {
+    public static char [][] seatingPeople ( char [][] layout ){
+        boolean end=false;
+        int i,j,contador;
+        for (i=0;i<layout.length;i++){
+            for (j=0;j<layout[i].length;j++){
+               if (layout[i][j]=='A'){layout[i][j]='#';}
+            }
+        }
+        while (!end){
+            for (i=0;i<layout.length;i++){
+                for (j=0;j<layout[i].length;j++){
+                    if (layout[i][j]=='#') {
+                        contador = 0;
+                        if (i == 0 && j == 0) {
+                            contador = 0;
+                        } else if (i == 0) {
+                            if (layout[i][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i][j + 1] == '#') {
+                                contador++;
+                            }
+
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+}
