63398614fa504dca82c0874dbf689d548e35195b
ikeroficcial <iker.calvo.gomez@udc.es>
2022-09-20 12:02:49 +0200

    Start e2

--- DS-43-iker.calvo.gomez_ivan.martinezes/src/e2/SocialDistance.java
+++ DS-43-iker.calvo.gomez_ivan.martinezes/src/e2/SocialDistance.java
@@ -2,6 +2,7 @@ package e2;
 
 public class SocialDistance {
     public static char [][] seatingPeople ( char [][] layout ){
+        char [][] layout2=null;
         boolean end=false;
         int i,j,contador;
         for (i=0;i<layout.length;i++){
@@ -10,13 +11,19 @@ public class SocialDistance {
             }
         }
         while (!end){
+            if (layout==layout2){end=true;}
             for (i=0;i<layout.length;i++){
                 for (j=0;j<layout[i].length;j++){
+
+                    layout2=layout;
                     if (layout[i][j]=='#') {
                         contador = 0;
                         if (i == 0 && j == 0) {
-                            contador = 0;
-                        } else if (i == 0) {
+                            contador = -1;
+                        }
+                        else if (i == 0 && j == (layout[i].length)-1) {
+                            contador = -1;
+                        }else if (i == 0) {
                             if (layout[i][j - 1] == '#') {
                                 contador++;
                             }
@@ -33,11 +40,232 @@ public class SocialDistance {
                                 contador++;
                             }
 
+                        } else if (i==(layout.length)-1 && j==0) {
+                            contador=-1;
+                        } else if (i==(layout.length)-1 && j==(layout[i].length)-1) {
+                        contador=-1;
+                    }else if (i == (layout.length)-1) {
+                            if (layout[i][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i][j + 1] == '#') {
+                                contador++;
+                            }
+                        } else if (j==0) {
+                            if (layout[i-1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i ][j+1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i+1][j] == '#') {
+                                contador++;
+                            }
+                        } else if (j==(layout[i].length-1)) {
+                            if (layout[i-1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i][j-1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i+1][j] == '#') {
+                                contador++;
+                            }
+                        }
+                        else {
+                            if (layout[i-1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j-1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i+1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i-1][j +1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j+1] == '#') {
+                                contador++;
+                            }
+                        }
+                        if (contador>=4){
+                            layout[i][j]='A';
                         }
                     }
+                    if (layout[i][j]=='A') {
+                        contador = 0;
+                        if (i == 0 && j == 0) {
+                            if (layout[i][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i+1][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i+1][j] == '#') {
+                                contador++;
+                            }
+                        }
+                        if (i == 0 && j == (layout[i].length)-1) {
+                            if (layout[i][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i+1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i+1][j] == '#') {
+                                contador++;
+                            }
+                        }else if (i == 0) {
+                            if (layout[i][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i][j + 1] == '#') {
+                                contador++;
+                            }
+
+                        } else if (i==(layout.length)-1 && j==0) {
+                            if (layout[i][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i-1][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i-1][j] == '#') {
+                                contador++;
+                            }
+                        } else if (i==(layout.length)-1 && j==(layout[i].length)-1) {
+                            if (layout[i][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i-1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i-1][j] == '#') {
+                                contador++;
+                            }
+                        }else if (i == (layout.length)-1) {
+                            if (layout[i][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i][j + 1] == '#') {
+                                contador++;
+                            }
+                        } else if (j==0) {
+                            if (layout[i-1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i ][j+1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i+1][j] == '#') {
+                                contador++;
+                            }
+                        } else if (j==(layout[i].length-1)) {
+                            if (layout[i-1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i][j-1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i+1][j] == '#') {
+                                contador++;
+                            }
+                        }
+                        else {
+                            if (layout[i-1][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i][j - 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j-1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i - 1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i+1][j] == '#') {
+                                contador++;
+                            }
+                            if (layout[i-1][j +1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i][j + 1] == '#') {
+                                contador++;
+                            }
+                            if (layout[i + 1][j+1] == '#') {
+                                contador++;
+                            }
+                        }
+                        if (contador<1){
+                            layout[i][j]='#';
+                        }
+                    }
+
                 }
             }
 
         }
-    }
+    return layout;}
 }
