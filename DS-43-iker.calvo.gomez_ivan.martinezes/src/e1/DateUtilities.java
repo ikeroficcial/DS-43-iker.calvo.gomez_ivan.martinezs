f6cf03222e6e840f4879dae3c52b3f14445d657c
ikeroficcial <iker.calvo.gomez@udc.es>
2022-09-20 11:03:54 +0200

    Start commit

--- DS-43-iker.calvo.gomez_ivan.martinezes/src/e1/DateUtilities.java
+++ DS-43-iker.calvo.gomez_ivan.martinezes/src/e1/DateUtilities.java
@@ -14,7 +14,7 @@ public class DateUtilities {
 
         switch (month){
             case 1,3,5,7,8,10,12: return 31;
-            case 2: if (isLeap(year)==true){return 29;}
+            case 2: if (isLeap(year)){return 29;}
                 else return 28;
 
             case 4,6,9,11: return 30;
@@ -26,51 +26,46 @@ public class DateUtilities {
         String fecha;
         String[] fecha2=dateText.split(" ");
         String mes=fecha2[0];
-        int mes2;
-        switch (mes){
-            case "January": mes2=1;break;
-            case "February": mes2=2;break;
-            case "March": mes2=3;break;
-            case "April": mes2=4;break;
-            case "May":mes2=5;break;
-            case "June":mes2=6;break;
-            case "July":mes2=7;break;
-            case "August": mes2=8;break;
-            case "September": mes2=9;break;
-            case "October":mes2=10;break;
-            case "November":mes2=11;break;
-            case "December":mes2=12;break;
-            default: throw new  IllegalArgumentException();
-        }
+        int mes2 = switch (mes) {
+            case "January" -> 1;
+            case "February" -> 2;
+            case "March" -> 3;
+            case "April" -> 4;
+            case "May" -> 5;
+            case "June" -> 6;
+            case "July" -> 7;
+            case "August" -> 8;
+            case "September" -> 9;
+            case "October" -> 10;
+            case "November" -> 11;
+            case "December" -> 12;
+            default -> throw new IllegalArgumentException();
+        };
         String dia=fecha2[1];
-        String año=fecha2[2];
+        String ano=fecha2[2];
         String dia2=dia.substring(0,dia.length()-1);
         if (mes2>9){
-            fecha=año+"-"+mes2+"-"+dia2;
+            fecha=ano+"-"+mes2+"-"+dia2;
         }
-        else fecha=año+"-0"+mes2+"-"+dia2;
+        else fecha=ano+"-0"+mes2+"-"+dia2;
         return fecha;
     }
     public static boolean checkISODate(String ISODate) {
         String[] fecha = ISODate.split("-");
-        String año = fecha[0];
-        int año1 = Integer.parseInt(año);
+        String ano = fecha[0];
+        int ano1 = Integer.parseInt(ano);
         String mes = fecha[1];
         int mes1 = Integer.parseInt(mes);
         String dia = fecha[2];
         int dia1 = Integer.parseInt(dia);
         System.out.println(mes1);
-        if (mes1>=13 && mes1 <=0) {
+        if (mes1>=13 || mes1 <=0) {
             return false;
         }
         else {
-            if (!(numberOfDays(mes1, año1) >= dia1)) {
+            if (!(numberOfDays(mes1, ano1) >= dia1)) {
                 return false;
-            } else {
-                if (mes1>12){
-                    return false;
-                }
-                else return true;
+            } else {return true;
             }
 
         }
